ded304c0114f8eee1d7ae2179f6f92c8573318a4
U-SBOYS3-PC\Admin <16994359+sboys3@users.noreply.github.com>
2020-01-31 16:54:14 -0600

    Bumped version

--- Plugin.cs
+++ Plugin.cs
@@ -19,7 +19,7 @@ namespace MusicSpatializer
 
         public Logger log;
         public const string Name = "MusicSpatializer";
-        public const  string Version = "1.0.0";
+        public const  string Version = "1.0.1";
         
 
         public void Init(Logger logger, [Config.Prefer("json")] IConfigProvider cfgProvider, PluginLoader.PluginMetadata metadata)
