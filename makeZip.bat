2def278be4a65a2ee84f1480c8523f5915eb3c3b
U-SBOYS3-PC\Admin <16994359+sboys3@users.noreply.github.com>
2020-02-10 16:26:38 -0600

    updated makeZip.bat

--- makeZip.bat
+++ makeZip.bat
@@ -2,7 +2,5 @@ mkdir tmpOut
 mkdir tmpOut\Plugins
 copy bin\x64\Release\MusicSpatializer.dll tmpOut\Plugins\MusicSpatializer.dll
 del plugin.zip
-cd tmpOut
-7z a -tZip -mm=Deflate -mfb=258 -mpass=15 -r ../plugin.zip *
-cd ../
+7z a -tZip -mm=Deflate -mfb=258 -mpass=15 -r plugin.zip ./tmpOut/*
 rmdir tmpOut /s /q
